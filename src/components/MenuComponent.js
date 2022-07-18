567b36fcfe4d4420d47a743e9dc32832d5a1c3e0
TrongNguyen <trongnguyennguyen767@gmail.com>
2022-07-18 12:35:40 +0700

    Single Page Applications Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderMenuItem ({dish, onClick}) {
-		
         return (
-            <Card
-                onClick={() => onClick(dish.id)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+            <Card>
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
         );
-		
     }
+
 
     const Menu = (props) => {
 
@@ -28,11 +30,24 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
                     {menu}
                 </div>
             </div>
         );
+        
+
 		
     }
 
 export default Menu;
+
